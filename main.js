4f3530ad26db513a5aafb84ce3bb9536678fffe8
mbecker <mary.becker@ct.gov>
2018-12-20 06:36:40 -0500

    updated bcg viz with semi circle for bio communities

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var lng= -72.65;
 var zoom= 9;
 
 var map = L.map('map', {
-    zoomControl: false,
+    zoomControl: true,
     attributionControl: false
 });
 
@@ -22,24 +22,67 @@ L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/
 
 L.control.attribution({position: 'bottomright'}).addTo(map);
 
+//in pixels, top left icon is drawn on lat,long
 var FishIcon = L.Icon.extend({
     options: {
-        iconSize:     [30, 20],
-        iconAnchor:   [20, 15],
-        popupAnchor:  [-3, -10]
+        iconSize:     [7, 14],
+        iconAnchor:   [7, 7],
+        popupAnchor:  [-7, -7]
     }
 });
 
-var redfishIcon = new FishIcon({iconUrl: 'redfish.png'}),
-    bluefishIcon = new FishIcon({iconUrl: 'bluefish.png'}),
-    yellowfishIcon = new FishIcon({iconUrl: 'yellowfish.png'});
+var BugIcon = L.Icon.extend({
+    options: {
+        iconSize:     [7, 14],
+        iconAnchor:   [0, 7],
+        popupAnchor:  [7, -1]
+    }
+});
 
-function getIcon(feature){
+var siteIcon = L.icon({
+    iconUrl: 'sitecircle.png',
+    iconSize:   [14,14]
+});
+
+//iconAnchor [downward_pixels, rightward_pixels]
+//left  rect/circle is positioned: iconAnchor: [-0.5*icon_height,-0.5*icon_width]
+//right rect/circle is positioned: iconAnchor: [-0.5*icon_height,-icon_width]
+
+var redfishIcon = new FishIcon({iconUrl: 'redfishcircle.png'}),
+    bluefishIcon = new FishIcon({iconUrl: 'bluefishcircle.png'}),
+    purplefishIcon = new FishIcon({iconUrl: 'purplefishcircle.png'});
+
+
+var redbugIcon = new BugIcon({iconUrl: 'redbugcircle.png'}),
+    bluebugIcon = new BugIcon({iconUrl: 'bluebugcircle.png'}),
+    purplebugIcon = new BugIcon({iconUrl: 'purplebugcircle.png'});
+
+/*function getIcon(feature){
     var icon;
-    if(feature.properties.AvgBCG <= 2) icon = bluefishIcon;
-    else if (feature.properties.AvgBCG <= 4) icon = yellowfishIcon;
-    else icon = redfishIcon;
+    if(feature.properties.AvgBCG <= 2 && feature.properties.SAMPLE == 'FISH') icon = bluefishIcon;
+    else if (feature.properties.AvgBCG <= 4 && feature.properties.SAMPLE == 'FISH') icon = purplefishIcon;
+    else if (feature.properties.AvgBCG <= 6 && feature.properties.SAMPLE == 'FISH') icon = redfishIcon;
+    else if (feature.properties.AvgBCG <= 2 && feature.properties.SAMPLE == 'BUG') icon = bluebugIcon;
+    else if (feature.properties.AvgBCG <= 4 && feature.properties.SAMPLE == 'BUG') icon = purplebugIcon;
+    else icon = redbugIcon;
     return icon;
+}*/
+
+
+function getFishIcon(feature){
+    var fishicon;
+    if(feature.properties.AvgBCG <= 2) fishicon = bluefishIcon;
+    else if (feature.properties.AvgBCG <= 4) fishicon = purplefishIcon;
+    else fishicon = redfishIcon;
+    return fishicon;
+}
+
+function getBugIcon(feature){
+    var bugicon;
+    if(feature.properties.AvgBCG <= 2) bugicon = bluebugIcon;
+    else if (feature.properties.AvgBCG <= 4) bugicon = purplebugIcon;
+    else bugicon = redbugIcon;
+    return bugicon;
 }
 
 /*$.getJSON("data/BCGsites.geojson",function(data){
@@ -52,7 +95,7 @@ function getIcon(feature){
     }).addTo(map);
 });*/
 
-$.getJSON("data/BCGsites.geojson",function(data){
+/*$.getJSON("data/BCGsites.geojson",function(data){
     var marker = L.geoJson(data ,{
         pointToLayer: function(feature,latlng){
         return L.marker (latlng,{icon: getIcon(feature),opacity: 0.6})
@@ -65,8 +108,50 @@ $.getJSON("data/BCGsites.geojson",function(data){
                 feature.properties.AvgBCG);
         }
     }).addTo(map);
+});*/
+
+var controlLayers = L.control.layers().addTo(map);
+
+$.getJSON("data/FishBCGsites.geojson",function(fishdata){
+    var fishmarker = L.geoJson(fishdata ,{
+        pointToLayer: function(feature,latlng){
+            return L.marker (latlng,{icon: getFishIcon(feature),opacity: 0.8})
+        },
+        onEachFeature: function (feature,marker) {
+            marker.bindPopup('<b>Stream: </b>' + feature.properties.Station_Name + '</br>' +
+                "<b>SID: </b>" + feature.properties.STA_SEQ + '</br>' +
+                "<b>SAMPLE: </b>Fish"+'</br>'+
+                "<b>BCG: </b>" +
+                feature.properties.AvgBCG);
+        }
+    }).addTo(map);
+    controlLayers.addOverlay(fishmarker,'Fish');
 });
 
+
+$.getJSON("data/BugBCGsites.geojson",function(bugdata){
+    var bugmarker = L.geoJson(bugdata ,{
+        pointToLayer: function(feature,latlng){
+            return L.marker (latlng,{icon: getBugIcon(feature),opacity: 0.8})
+        },
+        onEachFeature: function (feature,marker) {
+            marker.bindPopup('<b>Stream: </b>' + feature.properties.Station_Name + '</br>' +
+                "<b>SID: </b>" + feature.properties.STA_SEQ + '</br>' +
+                "<b>SAMPLE: </b>Bug"+'</br>'+
+                "<b>BCG: </b>" +
+                feature.properties.AvgBCG);
+        }
+    }).addTo(map);
+    controlLayers.addOverlay(bugmarker,'Bug');
+});
+
+$.getJSON("data/BCGsites.geojson",function(data) {
+    var marker = L.geoJson(data, {
+        pointToLayer: function (feature, latlng) {
+            return L.marker(latlng, {icon: siteIcon, opacity: 1})
+        }
+    }).addTo(map);
+});
 /*var omsOptions = {nearbyDistance:1};
 var oms = new OverlappingMarkerSpiderfier(map,omsOptions);
 
